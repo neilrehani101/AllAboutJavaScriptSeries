93ee846e807621c1ce05bca724b313a5f0edb331
TechBoy Corp <78197140+techboycorp1409@users.noreply.github.com>
2021-04-16 23:21:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// // Datatypes
+
+// var str = "Neil Rehani"; // String
+// var int = 11; // Int
+// var dec = 10.7; // decimal
+
+// // Printing to console
+// // 4 types
+
+// // LOG
+// console.log('this is a log');
+// // WARNING
+// console.warn('this is a warning')
+// // ERROR
+// console.error('this is an error');
+// // INFO
+// console.info('this is info');
+
+// str intpolation
+
+// const num1 = 12;
+// const num2 = 90;
+
+// console.info(`The sum = ${num1 + num2}`)
+
+// variables
+
+var num1 = "Hello!";
+// let == var - false but true
+
+let num2 = "Bye";
+
+num2 = "J"
+
+const pi = 3.14;
+pi = 3.45;
+
